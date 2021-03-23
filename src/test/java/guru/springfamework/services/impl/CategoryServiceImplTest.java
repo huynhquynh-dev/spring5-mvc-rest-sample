f85c70b7205bde5ad9a96a8356bc88e6bfd95e0f
huynhquynh-dev <huynhquynh-dev>
2021-03-23 14:51:09 +0700

    Added Create New Customer With Post

--- src/test/java/guru/springfamework/services/impl/CategoryServiceImplTest.java
+++ src/test/java/guru/springfamework/services/impl/CategoryServiceImplTest.java
@@ -1,9 +1,10 @@
-package guru.springfamework.services;
+package guru.springfamework.services.impl;
 
 import guru.springfamework.api.v1.mapper.CategoryMapper;
 import guru.springfamework.api.v1.model.CategoryDTO;
 import guru.springfamework.domain.Category;
 import guru.springfamework.repositories.CategoryRepository;
+import guru.springfamework.services.CategoryService;
 import guru.springfamework.services.impl.CategoryServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
